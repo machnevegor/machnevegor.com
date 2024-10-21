531c0eef66d545e3a5f3b63cf1614d20b569e1c5
machnevegor <egorikhelp@gmail.com>
2024-10-21 23:22:23 +0000

    feat: :sparkles: add logo, logogrid, avatar and favicon

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+export function Logo(props: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M13.2 1.5L0 21.3l1.8 1.2L13 5.7v9.6l2 0.6l6.8-10.2v9.6H24V2.1l-2-0.6l-6.8 10.2V2.1L13.2 1.5z" />
+    </svg>
+  );
+}
